30fe0352a94366902b5e860c7de3873a0cb2c84a
muneebwaqas416 <muneebwaqas416@gmail.com>
2023-12-03 03:21:55 +0500

    Muneeb

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from '../src/Components/SignUp/Signup'
 import Login from './Components/Login/Login'
 import Navabr from './Components/Navbar/Navabr'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <Sidebar></Sidebar>
         <Navabr></Navabr>
           <Routes>
             <Route path='/' Component={Signup}></Route>
