6475276b3eb57d4a01d6c89581b733b4d835ec8b
Hamzah-Manzoor <hamza88851@gmail.com>
2023-12-22 00:07:45 +0500

    Initial Design for the product page with dummy products

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from '../src/Components/SignUp/Signup'
 import Login from './Components/Login/Login'
+import Products from './Components/ProductsPage/ProductsMainPage'
 import Navabr from './Components/Navbar/Navabr'
 import Sidebar from './Components/Sidebar/Sidebar'
 
@@ -10,11 +11,12 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <Sidebar></Sidebar>
-        <Navabr></Navabr>
+      {/* <Sidebar></Sidebar> */}
+        {/* <Navabr></Navabr> */}
           <Routes>
             <Route path='/' Component={Signup}></Route>
             <Route path='/login' Component={Login}></Route>
+            <Route path='/products' Component={Products}></Route>
             
             
           
