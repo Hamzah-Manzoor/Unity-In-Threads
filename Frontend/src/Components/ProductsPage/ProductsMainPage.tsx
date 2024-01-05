561482afdc361aacc5aba5534319d75062297d8a
Hamzah-Manzoor <hamza88851@gmail.com>
2024-01-06 00:15:43 +0500

    Resolve Error

--- Frontend/src/Components/ProductsPage/ProductsMainPage.tsx
+++ Frontend/src/Components/ProductsPage/ProductsMainPage.tsx
@@ -101,7 +101,7 @@ const ProductPage: React.FC = () => {
 
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => console.log('Create Product clicked')}
+          onClick={handleCreateProduct}
         >
           Create Product
         </button>
