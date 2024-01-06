b4318e343e9c7431d4ce975a818a7e445f6b0157
Hamzah-Manzoor <hamza88851@gmail.com>
2024-01-06 16:14:05 +0500

    Improved the design of Add Product Page.

--- Frontend/src/Components/ProductsPage/AddProduct.tsx
+++ Frontend/src/Components/ProductsPage/AddProduct.tsx
@@ -21,88 +21,101 @@ const AddProduct: React.FC = () => {
 
   return (
     <div className="p-4">
-      <h2>Add Product</h2>
+      <p className="text-center font-bold text-4xl font-serif">ADD PRODUCT</p>
 
-      <div className="flex flex-col space-y-4">
-        <div className="flex flex-col">
-          <label htmlFor="productName" className="text-lg font-semibold mb-1">
-            Product Name
-          </label>
-          <input
-            type="text"
-            id="productName"
-            className="border border-gray-400 rounded px-3 py-2"
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
-          />
+      <div className="flex flex-col mx-7 space-y-4">
+
+        <div className="flex flex-col md:flex-row">
+
+            <div className="md:basis-1/2 md:mx-5">
+
+                <div className="flex flex-col">
+                <label htmlFor="productName" className="text-lg font-semibold mb-1">
+                    Product Name
+                </label>
+                <input
+                    type="text"
+                    id="productName"
+                    className="border border-gray-400 rounded px-3 py-2"
+                    value={productName}
+                    onChange={(e) => setProductName(e.target.value)}
+                />
+                </div>
+
+                <div className="flex flex-col">
+                <label htmlFor="productCode" className="text-lg font-semibold mb-1">
+                    Product Code
+                </label>
+                <input
+                    type="text"
+                    id="productCode"
+                    className="border border-gray-400 rounded px-3 py-2"
+                    value={productCode}
+                    onChange={(e) => setProductCode(e.target.value)}
+                />
+                </div>
+
+                <div className="flex flex-col">
+                <label htmlFor="productStock" className="text-lg font-semibold mb-1">
+                    Stock
+                </label>
+                <input
+                    type="number"
+                    id="productStock"
+                    className="border border-gray-400 rounded px-3 py-2"
+                    value={productStock}
+                    onChange={(e) => setProductStock(parseInt(e.target.value))}
+                />
+                </div>
+
+            </div>
+
+            <div className="md:basis-1/2 md:mx-5">
+
+                <div className="flex flex-col">
+                <label htmlFor="productColor" className="text-lg font-semibold mb-1">
+                    Color
+                </label>
+                <input
+                    type="text"
+                    id="productColor"
+                    className="border border-gray-400 rounded px-3 py-2"
+                    value={productColor}
+                    onChange={(e) => setProductColor(e.target.value)}
+                />
+                </div>
+
+                <div className="flex flex-col">
+                <label htmlFor="productType" className="text-lg font-semibold mb-1">
+                    Type
+                </label>
+                <input
+                    type="text"
+                    id="productType"
+                    className="border border-gray-400 rounded px-3 py-2"
+                    value={productType}
+                    onChange={(e) => setProductType(e.target.value)}
+                />
+                </div>
+
+                <div className="flex flex-col">
+                <label htmlFor="productCategory" className="text-lg font-semibold mb-1">
+                    Category
+                </label>
+                <input
+                    type="text"
+                    id="productCategory"
+                    className="border border-gray-400 rounded px-3 py-2"
+                    value={productCategory}
+                    onChange={(e) => setProductCategory(e.target.value)}
+                />
+                </div>
+
+            </div>
+
         </div>
 
-        <div className="flex flex-col">
-          <label htmlFor="productCode" className="text-lg font-semibold mb-1">
-            Product Code
-          </label>
-          <input
-            type="text"
-            id="productCode"
-            className="border border-gray-400 rounded px-3 py-2"
-            value={productCode}
-            onChange={(e) => setProductCode(e.target.value)}
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="productStock" className="text-lg font-semibold mb-1">
-            Stock
-          </label>
-          <input
-            type="number"
-            id="productStock"
-            className="border border-gray-400 rounded px-3 py-2"
-            value={productStock}
-            onChange={(e) => setProductStock(parseInt(e.target.value))}
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="productColor" className="text-lg font-semibold mb-1">
-            Color
-          </label>
-          <input
-            type="text"
-            id="productColor"
-            className="border border-gray-400 rounded px-3 py-2"
-            value={productColor}
-            onChange={(e) => setProductColor(e.target.value)}
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="productType" className="text-lg font-semibold mb-1">
-            Type
-          </label>
-          <input
-            type="text"
-            id="productType"
-            className="border border-gray-400 rounded px-3 py-2"
-            value={productType}
-            onChange={(e) => setProductType(e.target.value)}
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="productCategory" className="text-lg font-semibold mb-1">
-            Category
-          </label>
-          <input
-            type="text"
-            id="productCategory"
-            className="border border-gray-400 rounded px-3 py-2"
-            value={productCategory}
-            onChange={(e) => setProductCategory(e.target.value)}
-          />
-        </div>
-
-        <div className="flex flex-col">
+        <div className="flex flex-col md:mx-5">
           <label htmlFor="productImage" className="text-lg font-semibold mb-1">
             Product Image
           </label>
@@ -136,7 +149,7 @@ const AddProduct: React.FC = () => {
           )}
         </div>
 
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-3xl">
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 md:mx-auto md:w-7/12 lg:w-6/12 xl:w-5/12 rounded-3xl">
           Create Product
         </button>
       </div>
