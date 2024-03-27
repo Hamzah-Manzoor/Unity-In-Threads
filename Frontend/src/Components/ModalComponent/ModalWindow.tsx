2ea07ed3e207811e36a20b8deb1315a380392f3b
junaid-24 <l201269@lhr.nu.edu.pk>
2024-03-27 14:39:43 +0500

    Updtaed Modal Window

--- Frontend/src/Components/ModalComponent/ModalWindow.tsx
+++ Frontend/src/Components/ModalComponent/ModalWindow.tsx
@@ -11,9 +11,9 @@ const Modal: React.FC<ModalProps> = ({ isOpen, message, onClose }) => {
 
     return (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
-            <div className="bg-white p-8 rounded-lg text-center">
+            <div className="bg-white p-10 rounded-lg text-center">
                 <p>{message}</p>
-                <div className="mt-4">
+                <div className="mt-9">
                     <button onClick={onClose} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Close</button>
                 </div>
             </div>
